40fae8b1660114dc8d847f6285e55cac92b5cd00
Toru Nagashima <star.ctor@gmail.com>
2016-06-05 04:46:52 +0900

    New: `no-literal-call` rule

---
+++ docs/rules/no-literal-call.md
@@ -0,0 +1,23 @@
+# Disallow a call of a literal  (no-literal-call)
+
+A call of a literal is a valid syntax, but it would cause a runtime error.
+
+## Rule Details
+
+Examples of **incorrect** code for this rule:
+
+```js
+/*eslint no-literal-call: "error"*/
+
+123();
+"hello"();
+```
+
+Examples of **correct** code for this rule:
+
+```js
+/*eslint no-literal-call: "error"*/
+
+foo();
+(function() {})();
+```
---
+++ tests/lib/rules/no-literal-call.js
@@ -0,0 +1,67 @@
+/**
+ * @fileoverview Tests for no-literal-call rule.
+ * @author Toru Nagashima
+ */
+"use strict"
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-literal-call")
+var RuleTester = require("eslint").RuleTester
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester()
+
+ruleTester.run("no-literal-call", rule, {
+    valid: [
+        {code: "foo();"},
+        {code: "obj.foo();"},
+        {code: "(function() {})();"},
+        {code: "(() => 0)();", env: {es6: true}},
+        {code: "new foo();"},
+        {code: "new obj.foo();"},
+        {code: "new (function() {})();"},
+        {code: "new (class {})();", env: {es6: true}},
+        {code: "new (() => 0)();", env: {es6: true}},
+        {code: "foo``;", env: {es6: true}},
+        {code: "obj.foo``;", env: {es6: true}},
+        {code: "(function() {})``;", env: {es6: true}},
+        {code: "(() => 0)``;", env: {es6: true}},
+    ],
+    invalid: [
+        {code: "true();", errors: ["This is not a function."]},
+        {code: "false();", errors: ["This is not a function."]},
+        {code: "null();", errors: ["This is not a function."]},
+        {code: "100();", errors: ["This is not a function."]},
+        {code: "\"hello\"();", errors: ["This is not a function."]},
+        {code: "/abc/();", errors: ["This is not a function."]},
+        {code: "[1,2,3]();", errors: ["This is not a function."]},
+        {code: "({foo: 0})();", errors: ["This is not a function."]},
+        {code: "`hello`();", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "(class A {})();", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "new true();", errors: ["This is not a function."]},
+        {code: "new false();", errors: ["This is not a function."]},
+        {code: "new null();", errors: ["This is not a function."]},
+        {code: "new 100();", errors: ["This is not a function."]},
+        {code: "new \"hello\"();", errors: ["This is not a function."]},
+        {code: "new /abc/();", errors: ["This is not a function."]},
+        {code: "new [1,2,3]();", errors: ["This is not a function."]},
+        {code: "new ({foo: 0})();", errors: ["This is not a function."]},
+        {code: "new `hello`();", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "true``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "false``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "null``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "100``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "\"hello\"``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "/abc/``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "[1,2,3]``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "({foo: 0})``;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "`hello```;", env: {es6: true}, errors: ["This is not a function."]},
+        {code: "(class A {})``;", env: {es6: true}, errors: ["This is not a function."]},
+    ],
+})
