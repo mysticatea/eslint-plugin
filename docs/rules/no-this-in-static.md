5e9e4df02f5348367d1a5a259e8618b055646169
Toru Nagashima <star.ctor@gmail.com>
2016-10-09 01:07:04 +0900

    New: `no-this-in-static` rule (fixes #9)

---
+++ docs/rules/no-this-in-static.md
@@ -0,0 +1,53 @@
+# Disallow `this`/`super` in static methods (no-this-in-static)
+
+`this` keyword on static methods refers the class (the constructor) instance.
+However, probably it's confusing maintainers since this behavior is different to
+most other languages.
+
+This rule enforces a use of class itself to access static methods.
+
+## Rule Details
+
+Examples of **incorrect** code for this rule:
+
+```js
+/*eslint no-this-in-static: "error"*/
+
+class A {
+    static foo() {
+        doSomething()
+    }
+
+    static bar() {
+        this.foo()   //ERROR: Unexpected 'this'.
+    }
+}
+
+class B extends A {
+    static foo() {
+        super.foo()  //ERROR: Unexpected 'super'.
+    }
+}
+```
+
+Examples of **correct** code for this rule:
+
+```js
+/*eslint no-this-in-static: "error"*/
+
+class A {
+    static foo() {
+        doSomething()
+    }
+
+    static bar() {
+        A.foo()
+    }
+}
+
+class B extends A {
+    static foo() {
+        A.foo()
+    }
+}
+```
---
+++ tests/lib/rules/no-this-in-static.js
@@ -0,0 +1,47 @@
+/**
+ * @author Toru Nagashima
+ * @copyright 2016 Toru Nagashima. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+"use strict"
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const RuleTester = require("eslint").RuleTester
+const rule = require("../../../lib/rules/no-this-in-static")
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const tester = new RuleTester({parserOptions: {ecmaVersion: 6}})
+
+tester.run("no-this-in-static", rule, {
+    valid: [
+        "function foo() { this }",
+        "() => { this }",
+        "class A { constructor() { this } }",
+        "class A { foo() { this } }",
+        "class A { static foo() { function foo() { this } } }",
+    ],
+    invalid: [
+        {
+            code: "class A { static foo() { this } }",
+            errors: ["Unexpected 'this'."],
+        },
+        {
+            code: "class A { static foo() { () => { this } } }",
+            errors: ["Unexpected 'this'."],
+        },
+        {
+            code: "class A { static foo() { super.foo() } }",
+            errors: ["Unexpected 'super'."],
+        },
+        {
+            code: "class A { static foo() { () => { super.foo() } } }",
+            errors: ["Unexpected 'super'."],
+        },
+    ],
+})
