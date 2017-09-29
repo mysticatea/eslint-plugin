28156bad376b7d23725b2bde3c5a26ecb9e447f5
Thomas Grainger <tom@procensus.com>
2017-09-29 19:51:24 +0100

    configure ES2017 for no-useless-rest-spread

--- tests/lib/rules/no-useless-rest-spread.js
+++ tests/lib/rules/no-useless-rest-spread.js
@@ -17,7 +17,7 @@ const rule = require("../../../lib/rules/no-useless-rest-spread")
 
 const ruleTester = new RuleTester({
     parserOptions: {
-        ecmaVersion: 2015,
+        ecmaVersion: 2017,
         ecmaFeatures: { experimentalObjectRestSpread: true },
     },
 })
@@ -46,18 +46,16 @@ ruleTester.run("no-useless-rest-spread", rule, {
             output: "foo(...a, x, y, x)",
             errors: ["Unexpected a spread operator."],
         },
-        // Espree threw a syntax error.
-        // {
-        //     code: "let [a, ...[b, c, ...d]] = obj",
-        //     output: "let [a, b, c, ...d] = obj",
-        //     errors: ["Unexpected a rest operator."],
-        // },
-        // Espree threw a syntax error.
-        // {
-        //     code: "function foo(a, ...[b, c, ...d]) {}",
-        //     output: "function foo(a, b, c, ...d) {}",
-        //     errors: ["Unexpected a rest operator."],
-        // },
+        {
+            code: "let [a, ...[b, c, ...d]] = obj",
+            output: "let [a, b, c, ...d] = obj",
+            errors: ["Unexpected a rest operator."],
+        },
+        {
+            code: "function foo(a, ...[b, c, ...d]) {}",
+            output: "function foo(a, b, c, ...d) {}",
+            errors: ["Unexpected a rest operator."],
+        },
         {
             code: "let obj = {...{x, y, x}, ...b}",
             output: "let obj = {x, y, x, ...b}",
@@ -81,18 +79,16 @@ ruleTester.run("no-useless-rest-spread", rule, {
             output: "foo(...a, x, y, x)",
             errors: ["Unexpected a spread operator."],
         },
-        // Espree threw a syntax error.
-        // {
-        //     code: "let [a, ...[b, c, ]] = obj",
-        //     output: "let [a, b, c] = obj",
-        //     errors: ["Unexpected a rest operator."],
-        // },
-        // Espree threw a syntax error.
-        // {
-        //     code: "function foo(a, ...[b, c, ]) {}",
-        //     output: "function foo(a, b, c) {}",
-        //     errors: ["Unexpected a rest operator."],
-        // },
+        {
+            code: "let [a, ...[b, c, ]] = obj",
+            output: "let [a, b, c] = obj",
+            errors: ["Unexpected a rest operator."],
+        },
+        {
+            code: "function foo(a, ...[b, c, ]) {}",
+            output: "function foo(a, b, c) {}",
+            errors: ["Unexpected a rest operator."],
+        },
         {
             code: "let obj = {...{x, y, x, }, ...b}",
             output: "let obj = {x, y, x, ...b}",
@@ -116,18 +112,16 @@ ruleTester.run("no-useless-rest-spread", rule, {
             output: "foo(...a)",
             errors: ["Unexpected a spread operator."],
         },
-        // Espree threw a syntax error.
-        // {
-        //     code: "let [a, ...[]] = obj",
-        //     output: "let [a] = obj",
-        //     errors: ["Unexpected a rest operator."],
-        // },
-        // Espree threw a syntax error.
-        // {
-        //     code: "function foo(a, ...[b, c, ]) {}",
-        //     output: "function foo(a, b, c) {}",
-        //     errors: ["Unexpected a rest operator."],
-        // },
+        {
+            code: "let [a, ...[]] = obj",
+            output: "let [a] = obj",
+            errors: ["Unexpected a rest operator."],
+        },
+        {
+            code: "function foo(a, ...[b, c, ]) {}",
+            output: "function foo(a, b, c) {}",
+            errors: ["Unexpected a rest operator."],
+        },
         {
             code: "let obj = {...{}, ...b}",
             output: "let obj = { ...b}",
