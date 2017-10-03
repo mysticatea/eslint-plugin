f9e7959a5992c7d5bdcc68eca84c9ca8db1166e3
Toru Nagashima <star.ctor@gmail.com>
2017-10-03 13:18:40 +0900

    Chore: refactor `no-useless-rest-spread`

--- tests/lib/rules/no-useless-rest-spread.js
+++ tests/lib/rules/no-useless-rest-spread.js
@@ -26,12 +26,13 @@ ruleTester.run("no-useless-rest-spread", rule, {
     valid: [
         "let list = [...a, ...b]",
         "foo(...a, ...b)",
-        "let [a, b, c, ...d] = obj",
+        "let [a, b, c, ...d] = list",
         "function foo(a, b, c, ...d) {}",
         "let obj = {...a, ...b}",
         "let {a, ...b} = obj",
-
-        // those are invalid. I'll separate to another rule.
+        "let [a, ...{b, c}] = list",
+        "function foo(a, ...{b, c}) {}",
+        "foo(...{a}, ...{b})",
         "let list = [...{a}]",
         "let obj = {...[a]}",
     ],
@@ -39,99 +40,81 @@ ruleTester.run("no-useless-rest-spread", rule, {
         {
             code: "let list = [...[x, y, x], ...b]",
             output: "let list = [x, y, x, ...b]",
-            errors: ["Unexpected a spread operator."],
+            errors: ["Redundant spread element."],
         },
         {
             code: "foo(...a, ...[x, y, x])",
             output: "foo(...a, x, y, x)",
-            errors: ["Unexpected a spread operator."],
+            errors: ["Redundant spread element."],
         },
         {
             code: "let [a, ...[b, c, ...d]] = obj",
             output: "let [a, b, c, ...d] = obj",
-            errors: ["Unexpected a rest operator."],
+            errors: ["Redundant rest element."],
         },
         {
             code: "function foo(a, ...[b, c, ...d]) {}",
             output: "function foo(a, b, c, ...d) {}",
-            errors: ["Unexpected a rest operator."],
+            errors: ["Redundant rest parameter."],
         },
         {
             code: "let obj = {...{x, y, x}, ...b}",
             output: "let obj = {x, y, x, ...b}",
-            errors: ["Unexpected a spread property."],
+            errors: ["Redundant spread property."],
         },
-        // Espree threw a syntax error.
-        // {
-        //     code: "let {a, ...{b, c, ...d}} = obj",
-        //     output: "let {a, b, c, ...d} = obj",
-        //     errors: ["Unexpected a rest property."],
-        // },
 
         // Trailing commas
         {
             code: "let list = [...[x, y, x, ], ...b]",
             output: "let list = [x, y, x, ...b]",
-            errors: ["Unexpected a spread operator."],
+            errors: ["Redundant spread element."],
         },
         {
             code: "foo(...a, ...[x, y, x, ])",
             output: "foo(...a, x, y, x)",
-            errors: ["Unexpected a spread operator."],
+            errors: ["Redundant spread element."],
         },
         {
             code: "let [a, ...[b, c, ]] = obj",
             output: "let [a, b, c] = obj",
-            errors: ["Unexpected a rest operator."],
+            errors: ["Redundant rest element."],
         },
         {
             code: "function foo(a, ...[b, c, ]) {}",
             output: "function foo(a, b, c) {}",
-            errors: ["Unexpected a rest operator."],
+            errors: ["Redundant rest parameter."],
         },
         {
             code: "let obj = {...{x, y, x, }, ...b}",
             output: "let obj = {x, y, x, ...b}",
-            errors: ["Unexpected a spread property."],
+            errors: ["Redundant spread property."],
         },
-        // Espree threw a syntax error.
-        // {
-        //     code: "let {a, ...{b, c, }} = obj",
-        //     output: "let {a, b, c} = obj",
-        //     errors: ["Unexpected a rest property."],
-        // },
 
         // Empty literals
         {
             code: "let list = [...[], ...b]",
             output: "let list = [ ...b]",
-            errors: ["Unexpected a spread operator."],
+            errors: ["Redundant spread element."],
         },
         {
             code: "foo(...a, ...[])",
             output: "foo(...a)",
-            errors: ["Unexpected a spread operator."],
+            errors: ["Redundant spread element."],
         },
         {
             code: "let [a, ...[]] = obj",
             output: "let [a] = obj",
-            errors: ["Unexpected a rest operator."],
+            errors: ["Redundant rest element."],
         },
         {
             code: "function foo(a, ...[b, c, ]) {}",
             output: "function foo(a, b, c) {}",
-            errors: ["Unexpected a rest operator."],
+            errors: ["Redundant rest parameter."],
         },
         {
             code: "let obj = {...{}, ...b}",
             output: "let obj = { ...b}",
-            errors: ["Unexpected a spread property."],
+            errors: ["Redundant spread property."],
         },
-        // Espree threw a syntax error.
-        // {
-        //     code: "let {a, ...{}} = obj",
-        //     output: "let {a} = obj",
-        //     errors: ["Unexpected a rest property."],
-        // },
     ],
 })
